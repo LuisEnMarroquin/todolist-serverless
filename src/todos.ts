8bba2292cefaf4da8a644a10496fbbfdce1df386
LuisEnMarroquin <mluis651@gmail.com>
2022-03-04 23:26:24 -0600

    All requests wrapped in try catch statements

--- src/todos.ts
+++ src/todos.ts
@@ -5,129 +5,131 @@ import { APIGatewayProxyHandler } from "aws-lambda"
 const dynamoDb = new DynamoDB.DocumentClient()
 
 export const create: APIGatewayProxyHandler = (event, context, callback) => {
-  if (process.env.DYNAMO_TABLE_TODO === undefined) throw new Error("DYNAMO_TABLE_TODO is undefined")
+  try {
+    if (process.env.DYNAMO_TABLE_TODO === undefined) throw new Error("DYNAMO_TABLE_TODO is undefined")
 
-  const timestamp = Date.now()
-  const data = JSON.parse(event?.body ?? "")
+    const timestamp = Date.now()
+    console.log(`eventBody`, typeof event?.body, event?.body)
+    const data = JSON.parse(event?.body ?? "")
 
-  if (typeof data.text !== "string") {
-    console.error("Validation Failed")
-    callback(new Error("Couldn't create the todo item."))
-    return
-  }
-
-  const params = {
-    TableName: process.env.DYNAMO_TABLE_TODO,
-    Item: {
-      id: uuidv4(),
-      text: data.text,
-      checked: false,
-      createdAt: timestamp,
-      updatedAt: timestamp,
-    },
-  }
-
-  // write the todo to the database
-  dynamoDb.put(params, (error, result) => {
-    // handle potential errors
-    if (error) {
-      console.error(error)
+    if (typeof data.text !== "string") {
+      console.error("Validation Failed")
       callback(new Error("Couldn't create the todo item."))
       return
     }
 
-    // create a response
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
+    const params = {
+      TableName: process.env.DYNAMO_TABLE_TODO,
+      Item: {
+        id: uuidv4(),
+        text: data.text,
+        checked: false,
+        createdAt: timestamp,
+        updatedAt: timestamp,
+      },
+    }
+
+    // write the todo to the database
+    dynamoDb.put(params, (error, result) => {
+      if (error) throw error
+
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(params.Item),
+      })
     })
-  })
+  } catch (error) {
+    console.error(error)
+    callback(null, {
+      statusCode: error?.statusCode || 501,
+      headers: { "Content-Type": "text/plain" },
+      body: "Couldn't create the todo item",
+    })
+  }
 }
 
 export const read: APIGatewayProxyHandler = (event, context, callback) => {
-  if (process.env.DYNAMO_TABLE_TODO === undefined) throw new Error("DYNAMO_TABLE_TODO is undefined")
+  try {
+    if (process.env.DYNAMO_TABLE_TODO === undefined) throw new Error("DYNAMO_TABLE_TODO is undefined")
 
-  const params = {
-    TableName: process.env.DYNAMO_TABLE_TODO,
-    Key: {
-      id: event?.pathParameters?.id,
-    },
-  }
-
-  // fetch todo from the database
-  dynamoDb.get(params, (error, result) => {
-    // handle potential errors
-    if (error) {
-      console.error(error)
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { "Content-Type": "text/plain" },
-        body: "Couldn't fetch the todo item.",
-      })
-      return
+    const params = {
+      TableName: process.env.DYNAMO_TABLE_TODO,
+      Key: {
+        id: event?.pathParameters?.id,
+      },
     }
 
-    // create a response
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(result.Item),
+    // fetch todo from the database
+    dynamoDb.get(params, (error, result) => {
+      if (error) throw error
+
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(result.Item),
+      })
     })
-  })
+  } catch (error) {
+    console.error(error)
+    callback(null, {
+      statusCode: error?.statusCode || 501,
+      headers: { "Content-Type": "text/plain" },
+      body: "Couldn't fetch the todo item",
+    })
+  }
 }
 
 export const update: APIGatewayProxyHandler = (event, context, callback) => {
-  if (process.env.DYNAMO_TABLE_TODO === undefined) throw new Error("DYNAMO_TABLE_TODO is undefined")
+  try {
+    if (process.env.DYNAMO_TABLE_TODO === undefined) throw new Error("DYNAMO_TABLE_TODO is undefined")
 
-  const timestamp = Date.now()
-  const data = JSON.parse(event?.body ?? "")
+    const timestamp = Date.now()
+    const data = JSON.parse(event?.body ?? "")
 
-  // validation
-  if (typeof data.text !== "string" || typeof data.checked !== "boolean") {
-    console.error("Validation Failed")
-    callback(null, {
-      statusCode: 400,
-      headers: { "Content-Type": "text/plain" },
-      body: "Couldn't update the todo item.",
-    })
-    return
-  }
-
-  const params = {
-    TableName: process.env.DYNAMO_TABLE_TODO,
-    Key: {
-      id: event?.pathParameters?.id,
-    },
-    ExpressionAttributeNames: {
-      "#todo_text": "text",
-    },
-    ExpressionAttributeValues: {
-      ":text": data.text,
-      ":checked": data.checked,
-      ":updatedAt": timestamp,
-    },
-    UpdateExpression: "SET #todo_text = :text, checked = :checked, updatedAt = :updatedAt",
-    ReturnValues: "ALL_NEW",
-  }
-
-  // update the todo in the database
-  dynamoDb.update(params, (error, result) => {
-    // handle potential errors
-    if (error) {
-      console.error(error)
+    // validation
+    if (typeof data.text !== "string" || typeof data.checked !== "boolean") {
+      console.error("Validation Failed")
       callback(null, {
-        statusCode: error.statusCode || 501,
+        statusCode: 400,
         headers: { "Content-Type": "text/plain" },
-        body: "Couldn't fetch the todo item.",
+        body: "Couldn't update the todo item.",
       })
       return
     }
 
-    // create a response
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(result.Attributes),
+    const params = {
+      TableName: process.env.DYNAMO_TABLE_TODO,
+      Key: {
+        id: event?.pathParameters?.id,
+      },
+      ExpressionAttributeNames: {
+        "#todo_text": "text",
+      },
+      ExpressionAttributeValues: {
+        ":text": data.text,
+        ":checked": data.checked,
+        ":updatedAt": timestamp,
+      },
+      UpdateExpression: "SET #todo_text = :text, checked = :checked, updatedAt = :updatedAt",
+      ReturnValues: "ALL_NEW",
+    }
+
+    // update the todo in the database
+    dynamoDb.update(params, (error, result) => {
+      if (error) throw error
+
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(result.Attributes),
+      })
     })
-  })
+  } catch (error) {
+    console.error(error)
+    callback(null, {
+      statusCode: error?.statusCode || 501,
+      headers: { "Content-Type": "text/plain" },
+      body: "Couldn't fetch the todo item",
+    })
+  }
 }
 
 export const list: APIGatewayProxyHandler = (event, context, callback) => {
@@ -140,10 +142,8 @@ export const list: APIGatewayProxyHandler = (event, context, callback) => {
 
     // For production workloads you should design your tables and indexes so that your apps can use Query instead of Scan
     dynamoDb.scan(params, (error, result) => {
-      // handle potential errors
       if (error) throw error
 
-      // create a response
       callback(null, {
         statusCode: 200,
         body: JSON.stringify(result.Items),
@@ -154,7 +154,7 @@ export const list: APIGatewayProxyHandler = (event, context, callback) => {
     callback(null, {
       statusCode: error?.statusCode || 501,
       headers: { "Content-Type": "text/plain" },
-      body: "Couldn't fetch the todo items.",
+      body: "Couldn't fetch the todo items",
     })
   }
 }
